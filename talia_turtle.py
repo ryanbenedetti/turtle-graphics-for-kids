70c8cfe5adfa98e91e57d4dbe8f79b1d6cba24c7
ryanbenedetti <rhino.j.benedetti@gmail.com>
2015-06-07 11:45:55 -0700

    Added simple talia program

---
+++ talia_turtle.py
@@ -0,0 +1,7 @@
+import turtle
+
+turtle.bgcolor("black")
+talia = turtle.Pen()
+talia.shape("turtle")
+talia.pencolor("green")
+talia.circle(50)
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import turtle
 
-#Crazy bug eyes pattern by Ryan Benedetti,6/7/2015
+#Circles into 3D-ish objects by Ryan Benedetti, 6/7/2015
 
 turtle.bgcolor("black")
+
 tony = turtle.Pen()
+tony.shape("turtle")
 tony.pencolor("green")
 for i in range(360):
  #as the loop repeats, i will count up and the circles will grow
